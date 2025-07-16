8d995b017c17248c51c0b0ee54351e2510f99330
DSHE_ZHBS <hds1988@126.com>
2025-07-16 09:40:28 +0800

    feat:add version
    Signed-off-by: DSHE_ZHBS <hds1988@126.com>

--- CO2-CO-Sensor/Form1.Designer.cs
+++ CO2-CO-Sensor/Form1.Designer.cs
@@ -662,7 +662,7 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "陕西中核贝森电子科技有限公司---传感器测试软件";
+            this.Text = "陕西中核贝森电子科技有限公司---传感器测试软件V1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
